33ee2d1e4a4c8c891620dbcc196a951053387778
Mustafa Bugra <mustafakaraturk66@gmail.com>
2024-04-02 16:00:02 +0300

    Düzenleme

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from settings import *
 from map import *
 from player import *
-from raycastimg import *
+from raycasting import *
 class Game:
     def __init__(self):
         pg.init()
@@ -16,12 +16,12 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
-        self.raycastimg = RayCasting(self)
+        self.raycasting = RayCasting(self)
 
 
     def update(self):
         self.player.update()
-        self.raycastimg.update()
+        self.raycasting.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
