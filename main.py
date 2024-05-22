48101c23a8dd2e5bb04cb0252efa37bcf14dcb82
sntrsultan <163646822+sntrsultan@users.noreply.github.com>
2024-05-22 13:44:19 +0300

    npc

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from object_renderer import *
 from sprite_object import *
 from object_handler import *
 from weapon import *
+from sound import *
+from pathfinding import *
 
 class Game:
     def __init__(self):
@@ -16,6 +18,9 @@ class Game:
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
 
 
@@ -26,6 +31,8 @@ class Game:
         self.raycasting = RayCasting(self)
         self.object_handler = ObjectHandler(self)
         self.weapon = Weapon(self)
+        self.sound = Sound(self)
+        self.pathfinding = PathFinding(self)
 
 
     def update(self):
@@ -45,11 +52,14 @@ class Game:
         #self.player.draw()
 
     def check_events(self):
+        self.global_trigger = False
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
-
+            elif event.type == self.global_event:
+                self.global_trigger = True    
+            self.player.single_fire_event(event)
 
 
     def run(self):
