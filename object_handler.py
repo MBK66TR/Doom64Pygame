9ec5f4d680b42d345edbf89254f0e260cd9663ec
sntrsultan <163646822+sntrsultan@users.noreply.github.com>
2024-05-22 13:54:43 +0300

    Update object_handler.py

--- object_handler.py
+++ object_handler.py
@@ -7,9 +7,13 @@ class ObjectHandler:
     def __init__(self, game):
         self.game = game
         self.sprite_list = []
+        self.npc_list = []
+        self.npc_sprite_path = 'resources/sprites/npc/'
         self.static_sprite_path = 'resources/sprites/static_sprites/'
         self.anim_sprite_path = 'resources/sprites/animated_sprites/'
         add_sprite = self.add_sprite
+        add_npc = self.add_npc
+        self.npc_positions = {}
 
         # sprite map
         add_sprite(SpriteObject(game))
@@ -46,11 +50,17 @@ class ObjectHandler:
         # add_npc(CacoDemonNPC(game, pos=(5.5, 14.5)))
         # add_npc(CacoDemonNPC(game, pos=(5.5, 16.5)))
         # add_npc(CyberDemonNPC(game, pos=(14.5, 25.5)))
+        add_npc(NPC(game))
+        add_npc(NPC(game, pos=(11.5, 4.5)))
 
 
     def update(self):
+        self.npc_positions = {npc.map_pos for npc in self.npc_list if npc.alive}
         [sprite.update() for sprite in self.sprite_list]
+        [npc.update() for npc in self.npc_list]
 
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
 
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
