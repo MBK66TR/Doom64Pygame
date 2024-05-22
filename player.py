fab3baa962fac713b16af8ab12dd93581a4bd757
sntrsultan <163646822+sntrsultan@users.noreply.github.com>
2024-05-22 13:55:34 +0300

    Update player.py

--- player.py
+++ player.py
@@ -7,6 +7,36 @@ class Player:
         self.game = game
         self.x, self.y = PLAYER_POS
         self.angle = PLAYER_ANGLE
+        self.shot = False
+        self.health = PLAYER_MAX_HEALTH
+        self.rel = 0
+        self.health_recovery_delay = 700
+        self.time_prev = pg.time.get_ticks()
+
+    def recover_health(self):
+        if self.check_health_recovery_delay() and self.health < PLAYER_MAX_HEALTH:
+            self.health += 1    
+
+    def check_game_over(self):
+        if self.health <1:
+            self.game.object_renderer.game_over()
+            pg.display.flip()
+            pg.time.delay(1500)
+            self.game.new_game()   
+
+    def get_damage(self, game):
+        self.health -= damage   
+        self.game.object_renderer.player_damage() 
+        self.game.sound.player_pain.play()
+        self.check_game_over()
+
+    def single_fire_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1 and not self.shot and not self.game.weapon.reloading:
+                self.game.sound.shotgun.play()
+                self.shot = True
+                self.game.weapon.reloading = True
+
 
         # diagonal movement correction
         self.diag_move_corr = 1 / math.sqrt(2)
@@ -87,6 +117,7 @@ class Player:
     def update(self):
         self.movement()
         self.mouse_control()
+        self.recorver_health()
 
     @property
     def pos(self):
